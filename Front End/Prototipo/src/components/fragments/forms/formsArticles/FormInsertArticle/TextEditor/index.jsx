ce8db19ccf2f35285d897ad8a0f0fb37a7ac925f
Jonas Alves de Souza <souza.jonas.a@gmail.com>
2024-05-13 17:16:05 -0300

    insert Editor Text

--- Front End/Prototipo/src/components/fragments/forms/formsArticles/FormInsertArticle/TextEditor/index.jsx
+++ Front End/Prototipo/src/components/fragments/forms/formsArticles/FormInsertArticle/TextEditor/index.jsx
@@ -1,34 +1,38 @@
-import { useRef, useState } from "react";
-import ReactQuill from "react-quill";
+import React, { useState } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
 
 export const TextEditor = ({ setEditorContent }) => {
   const [text, setText] = useState('');
-  const reactQuillRef = useRef(null); // Usar useRef para a referência
 
-  const handleChange = (content) => {
+  const handleEditorChange = (content, editor) => {
     setText(content);
     setEditorContent(content);
   };
 
   return (
-    <div>
-      <ReactQuill
-        theme="snow"
-        value={text}
-        onChange={handleChange}
-        modules={{
-          toolbar: [
-            [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
-            [{ 'size': ['small', false, 'large', 'huge'] }],
-            ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-            [{ 'align': [] }],
-            ['link'],
-            [{ 'color': [] }],
-            [{ 'code-block': 'code' }], // Botão de bloco de código
-          ],
-        }}
-        ref={reactQuillRef} // Definir a referência diretamente
-      />
-    </div>
+    <Editor
+      apiKey="wglqt5j1s1zt8d5f7mqju5hldqn3st3tu2r0m49dxvehcs01"
+      initialValue="<p>Conteúdo inicial</p>"
+      init={{
+        height: 500,
+        menubar: true,
+        plugins: [
+          'advlist autolink lists link image charmap print preview anchor',
+          'searchreplace visualblocks code fullscreen',
+          'insertdatetime media table paste code help wordcount',
+        ],
+        toolbar:
+          'undo redo | formatselect | bold italic backcolor | \
+          alignleft aligncenter alignright alignjustify | \
+          bullist numlist outdent indent | removeformat | code',
+        codesample_languages: [
+          { text: 'JavaScript', value: 'javascript' },
+          { text: 'HTML', value: 'html' },
+          // Adicione outras linguagens conforme necessário
+        ],
+        codesample_content_css: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.7.2/styles/default.min.css',
+      }}
+      onEditorChange={handleEditorChange}
+    />
   );
 };
