793eeb4152d4df9869ea64633993a3827ba768b1
Jonas Alves de Souza <souza.jonas.a@gmail.com>
2024-11-20 16:50:39 -0300

    Refatoring Homepage insert sniper

---
+++ FrontEnd/src/components/fragments/SpinnerColorRing/index.jsx
@@ -0,0 +1,23 @@
+import { ColorRing } from 'react-loader-spinner';
+
+import styles from './style.module.scss';
+
+export const SpinnerColorRing = ({ isResponsive }) => {
+	const classSpiner = `${isResponsive ? styles.vertical : styles.horizontal}`;
+
+	console.log(isResponsive);
+
+	return (
+		<div className={classSpiner}>
+			<ColorRing
+				visible={true}
+				height="80"
+				width="80"
+				ariaLabel="color-ring-loading"
+				wrapperStyle={{}}
+				wrapperClass="color-ring-wrapper"
+				colors={['#404449', '#2d3136', '#76787b', '#404449', '#404449']}
+			/>
+		</div>
+	);
+};
---
+++ FrontEnd/src/data/SectionPageHome/index.jsx
@@ -0,0 +1,13 @@
+import {
+	SectionBannerHomePage,
+	SectionAboutHomePage,
+	SectionMeEmail,
+	SectionWorkplace,
+} from '../../components/sectionsPage/sectionsHomePage';
+
+export const SectionsPageHome = [
+	<SectionBannerHomePage key="banner" />,
+	<SectionAboutHomePage key="about" />,
+	<SectionWorkplace key="workplace" />,
+	<SectionMeEmail key="email" />,
+];
