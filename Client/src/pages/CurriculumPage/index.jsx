eeffff3696af830d38a0f4b06454c71fce688b74
Jonasalvesdesouza <souza.jonas.a@gmail.com>
2026-02-19 02:53:46 -0300

    Refining Header

--- Client/src/pages/CurriculumPage/index.jsx
+++ Client/src/pages/CurriculumPage/index.jsx
@@ -1,5 +1,7 @@
 import { Header } from '../../components/fragments';
 import { useSectionVisibilityCurriculum } from '../../hooks';
+import { useSticky } from '../../hooks';
+
 import { SectionsPageCurriculum } from '../../data';
 
 import styles from './styles.module.scss';
@@ -7,16 +9,20 @@ import styles from './styles.module.scss';
 export const CurriculumPage = () => {
 	const sections = SectionsPageCurriculum;
 	const { headerClass, sectionRefs } = useSectionVisibilityCurriculum(sections);
+	  const [isSticky, filterRef] = useSticky();
 
 	return (
 		<>
-			<div className={`${styles.curriculumContainer}`}>
-				<Header headerClass={headerClass} />
-				<main>
+			<div 
+				className={`${styles.curriculumContainer}`}
+				isSticky={isSticky}
+			>
+				<Header headerClass={headerClass} isSticky={isSticky} />
+				<main ref={filterRef}>
 					<div ref={sectionRefs[0][0]} className={`${styles.leftContainer}`}>
 						{sections[0].component}
 					</div>
-					<div ref={sectionRefs[1][0]} className={`${styles.rightContainer}`}>
+					<div  ref={sectionRefs[1][0]} className={`${styles.rightContainer}`}>
 						{sections[1].component}
 					</div>
 				</main>
